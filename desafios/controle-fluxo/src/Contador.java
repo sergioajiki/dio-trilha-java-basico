08be1a1a1a35a0208d23435922f625a5e4efd68b
Sergio Ajiki <sergioajk@hotmail.com>
2024-05-07 19:36:44 -0300

    entregando desafio Contador

--- desafios/controle-fluxo/src/Contador.java
+++ desafios/controle-fluxo/src/Contador.java
@@ -13,15 +13,23 @@ public class Contador {
             //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
 
-        }catch (? exception) {
+        } catch (ParametrosInvalidosException exception) {
             //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
-
     }
-    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
 
         int contagem = parametroDois - parametroUm;
         //realizar o for para imprimir os números com base na variável contagem
+        for (int i = 1; i <= contagem; i++)
+        {
+            System.out.println("Imprimindo o número " + i);
+        }
     }
 }
