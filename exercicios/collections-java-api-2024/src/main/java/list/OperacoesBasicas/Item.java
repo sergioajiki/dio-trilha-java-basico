811a69c1d8be110602a81c105a1b15ea39a829fe
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-05 16:27:02 -0300

    feat: Item

---
+++ exercicios/collections-java-api-2024/src/main/java/list/OperacoesBasicas/Item.java
@@ -0,0 +1,13 @@
+package main.java.list.OperacoesBasicas;
+
+public class Item {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Item(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+}
