5aea3140ee456059db28cf1aafef150316a60f3e
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-05 12:10:15 -0300

    feat: Tarefa

--- exercicios/collections-java-api-2024/src/main/java/list/ListaTarefa.java
+++
@@ -1,4 +0,0 @@
-package main.java.list;
-
-public class ListaTarefa {
-}
---
+++ exercicios/collections-java-api-2024/src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,14 @@
+package main.java.list.OperacoesBasicas;
+
+public class Tarefa {
+    //Cada tarefa é representada por uma classe chamada "Tarefa" que possui um atributo de descrição
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
