c4b6b18e35a013469903a956e060b1b0f7fa4378
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-06 13:57:07 -0300

    OrdenacaoNumeros resolvido

--- exercicios/collections-java-api-2024/src/main/java/list/Ordenacao/OrdenacaoNumeros.java
+++ exercicios/collections-java-api-2024/src/main/java/list/Ordenacao/OrdenacaoNumeros.java
@@ -18,15 +18,33 @@ public class OrdenacaoNumeros {
 
     //Ordena os números da lista em ordem ascendente usando a interface Comparable e a class Collections.
     public List<Integer> ordenarAscendente() {
-        List<Integer> numAscendente = new ArrayList<>();
+        List<Integer> numAscendente = new ArrayList<>(numeros);
         Collections.sort(numAscendente);
         return numAscendente;
     }
 
     //Ordena os números da lista em ordem descendente usando um Comparable e a class Collections.
     public List<Integer> ordenarDescendente() {
-        List<Integer> numDescendente = new ArrayList<>();
+        List<Integer> numDescendente = new ArrayList<>(numeros);
         Collections.sort(numDescendente, Collections.reverseOrder());
         return numDescendente;
+    }
+
+    @Override
+    public String toString() {
+        return "Numeros: " + numeros;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoNumeros ordenacaoNumeros = new OrdenacaoNumeros();
+        ordenacaoNumeros.adicionarNumero(2);
+        ordenacaoNumeros.adicionarNumero(5);
+        ordenacaoNumeros.adicionarNumero(20);
+        ordenacaoNumeros.adicionarNumero(1);
+        ordenacaoNumeros.adicionarNumero(7);
+        ordenacaoNumeros.adicionarNumero(-7);
+        System.out.println(ordenacaoNumeros);
+        System.out.println("Números em ordem ascendente: " + ordenacaoNumeros.ordenarAscendente());
+        System.out.println("Números em ordem descendente: " + ordenacaoNumeros.ordenarDescendente());
     }
 }
