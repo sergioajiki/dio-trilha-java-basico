b7b6a3bccdb69efd8060dbec5832227e7d76b80f
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-06 10:28:34 -0300

    SomaNumeros resolvido
    Signed-off-by: Sergio Ajiki <sergioajk@hotmail.com>

--- exercicios/collections-java-api-2024/src/main/java/list/Pesquisa/SomaNumeros.java
+++ exercicios/collections-java-api-2024/src/main/java/list/Pesquisa/SomaNumeros.java
@@ -53,4 +53,19 @@ public class SomaNumeros {
     public void exibirNumeros() {
         System.out.println(this.numeros);
     }
+
+    public static void main(String[] args) {
+        SomaNumeros somaNumeros = new SomaNumeros();
+        somaNumeros.adicionarNumero(1);
+        somaNumeros.adicionarNumero(10);
+        somaNumeros.adicionarNumero(5);
+        somaNumeros.adicionarNumero(3);
+        somaNumeros.adicionarNumero(6);
+        somaNumeros.adicionarNumero(7);
+        somaNumeros.adicionarNumero(-10);
+        somaNumeros.exibirNumeros();
+        System.out.println("Soma dos números = " + somaNumeros.calcularSoma());
+        System.out.println("Maior número = " + somaNumeros.encontrarMaiorNumero());
+        System.out.println("Menor número = " + somaNumeros.encontrarMenorNumero());
+    }
 }
