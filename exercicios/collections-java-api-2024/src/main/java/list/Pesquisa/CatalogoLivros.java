531597715411808a865a667e65de6e1d9f0df5b3
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-05 22:07:31 -0300

    feat: CatalogoLivro

---
+++ exercicios/collections-java-api-2024/src/main/java/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,58 @@
+package main.java.list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    //atributos
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    //Adiciona um livro ao catálogo.
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    //Pesquisa livros por autor e retorna uma lista com os livros encontrados.
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    //Pesquisa livros publicados em um determinado intervalo de anos e retorna uma lista com os livros encontrados.
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livrosPorIntervaloAnos;
+    }
+
+    //Pesquisa livros por título e retorna o primeiro livro encontrado.
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+}
