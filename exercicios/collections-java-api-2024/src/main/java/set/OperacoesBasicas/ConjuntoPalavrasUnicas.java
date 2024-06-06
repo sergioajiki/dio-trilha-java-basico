6ad8e7dc3301d6338a3cb8fa5ee63298c9659c85
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-06 16:10:33 -0300

    feat: ConjuntoPalavrasUnicas

--- exercicios/collections-java-api-2024/src/main/java/set/OperacoesBasicas/ConjuntoPalavrasUnicas.java
+++ exercicios/collections-java-api-2024/src/main/java/set/OperacoesBasicas/ConjuntoPalavrasUnicas.java
@@ -1,4 +1,34 @@
 package main.java.set.OperacoesBasicas;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ConjuntoPalavrasUnicas {
+    private Set<String> palavraSet;
+
+    public ConjuntoPalavrasUnicas() {
+        this.palavraSet = new HashSet<>();
+    }
+
+    //Adiciona uma palavra ao conjunto.
+    public void adicionarPalavra(String palavra) {
+        palavraSet.add(palavra);
+    }
+
+    //Remove uma palavra do conjunto.
+    public void removerPalavra(String palavra) {
+        palavraSet.remove(palavra);
+    }
+
+    //Verifica se uma palavra está presente no conjunto.
+    public boolean verificarPalavra(String palavra) {
+        return palavraSet.contains(palavra);
+    }
+
+    //Exibe todas as palavras únicas do conjunto.
+    public void exibirPalavrasUnicas() {
+        for(String palavra : palavraSet)
+            System.out.println(palavra);
+    }
+
 }
