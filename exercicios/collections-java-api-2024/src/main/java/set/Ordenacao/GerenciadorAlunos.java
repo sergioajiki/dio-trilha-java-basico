a72f5cf0ff9837626a7b0c293d8a822411ec55e9
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-06 23:21:07 -0300

    GerenciadorAlunos Resolvido

--- exercicios/collections-java-api-2024/src/main/java/set/Ordenacao/GerenciadorAlunos.java
+++ exercicios/collections-java-api-2024/src/main/java/set/Ordenacao/GerenciadorAlunos.java
@@ -36,8 +36,39 @@ public class GerenciadorAlunos {
 
     //Exibe todos os alunos do conjunto.
     public void exibirAlunos() {
-        for(Aluno aluno : alunoSet) {
+        for (Aluno aluno : alunoSet) {
             System.out.println(aluno);
         }
+    }
+
+    public static void main(String[] args) {
+        GerenciadorAlunos gerenciador = new GerenciadorAlunos();
+
+        gerenciador.adicionarAluno("Aluno7", 1L, 8.5);
+        gerenciador.adicionarAluno("Aluno2", 2L, 7.0);
+        gerenciador.adicionarAluno("Aluno8", 3L, 9.0);
+        gerenciador.adicionarAluno("Aluno99", 4L, 5.0);
+        gerenciador.adicionarAluno("Aluno4", 3L, 6.5);
+        gerenciador.adicionarAluno("Aluno3", 5L, 6.5);
+
+        System.out.println("Todos os alunos:");
+        gerenciador.exibirAlunos();
+
+        System.out.println("\nAlunos por nome:");
+        for (Aluno aluno : gerenciador.exibirAlunosPorNome()) {
+            System.out.println(aluno);
+        }
+
+        System.out.println("\nAlunos por nota:");
+        for (Aluno aluno : gerenciador.exibirAlunosPorNota()) {
+            System.out.println(aluno);
+        }
+
+        gerenciador.removerAluno(2L);
+        System.out.println("\nAlunos após remover o aluno com matrícula 2:");
+        gerenciador.exibirAlunos();
+
+        System.out.println("Todos os alunos:");
+        gerenciador.exibirAlunos();
     }
 }
