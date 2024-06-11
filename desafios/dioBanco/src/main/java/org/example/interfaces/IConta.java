cdbea30cd086595903d44c0d7079e2b1fcade9b3
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-11 16:23:21 -0300

    feat<interface>: IConta - interface para Conta

---
+++ desafios/dioBanco/src/main/java/org/example/interfaces/IConta.java
@@ -0,0 +1,8 @@
+package org.example.interfaces;
+
+public interface IConta {
+    void sacar(double valor);
+    void depositar(double valor);
+    void tranferir(double valor, IConta contaDestino);
+    void imprimirExtrato();
+}
