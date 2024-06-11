09eaf403de14c841d3f7d19173188b5c37e4296c
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-11 17:00:30 -0300

    feat:Cliente

---
+++ desafios/dioBanco/src/main/java/org/example/Cliente.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
