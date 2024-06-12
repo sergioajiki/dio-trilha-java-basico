3786b96423e09a0086f9d56f5214dbec87f53f7b
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-12 11:54:09 -0300

    feat<Dev>: completando metodos

--- desafios/desafio-poo-dio/src/br/com/dio/desafio/dominio/Dev.java
+++ desafios/desafio-poo-dio/src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,9 +10,27 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp) {}
-    public void progredir() {}
-    public void calcularTotalXp() {}
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
+
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
+
+    public double calcularTotalXp() {
+        return this.conteudosConcluidos
+                .stream()
+                .mapToDouble(conteudo -> conteudo.calcularXp())
+                .sum();
+    }
 
     public String getNome() {
         return nome;
@@ -41,7 +60,9 @@ public class Dev {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Dev dev)) return false;
-        return Objects.equals(getNome(), dev.getNome()) && Objects.equals(getConteudosInscritos(), dev.getConteudosInscritos()) && Objects.equals(getConteudosConcluidos(), dev.getConteudosConcluidos());
+        return Objects.equals(getNome(), dev.getNome())
+                && Objects.equals(getConteudosInscritos(), dev.getConteudosInscritos())
+                && Objects.equals(getConteudosConcluidos(), dev.getConteudosConcluidos());
     }
 
     @Override
