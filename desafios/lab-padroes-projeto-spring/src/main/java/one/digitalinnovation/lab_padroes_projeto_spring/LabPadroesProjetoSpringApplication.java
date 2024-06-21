367e3db1f194047ac74bb0ffdcd138b408adc5d9
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-21 12:32:29 -0300

    initial commit Projeto Padrões de Projetos em uma API Rest

---
+++ desafios/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.lab_padroes_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Projeto Spring Boot gerado com Spring Initializr
+ * Dependências selecionadas:
+ * Spring Web
+ * Spring Data JPA
+ * H2 Database
+ * OpenFeign
+ */
+
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
