dc4374030add71ebd16bc909501119143c2a4fb1
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-24 19:36:59 -0300

    commit inicial Desafio Deploy Api Santander Bootcamp

---
+++ desafios/desafio-santander-api-deploy-2024/src/main/java/dio/me/desafio_santander_api_deploy_2024/DesafioSantanderApiDeploy2024Application.java
@@ -0,0 +1,13 @@
+package dio.me.desafio_santander_api_deploy_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSantanderApiDeploy2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSantanderApiDeploy2024Application.class, args);
+	}
+
+}
---
+++ desafios/desafio-santander-api-deploy-2024/src/test/java/dio/me/desafio_santander_api_deploy_2024/DesafioSantanderApiDeploy2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me.desafio_santander_api_deploy_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSantanderApiDeploy2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
