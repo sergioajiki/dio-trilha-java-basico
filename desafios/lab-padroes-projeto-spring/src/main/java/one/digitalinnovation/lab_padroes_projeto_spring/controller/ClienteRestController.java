ebfe0164034d3d1278ca5d9594611a003d28647b
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-24 17:36:24 -0300

    feat<ClienteRestController>: refaorado

--- desafios/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/controller/ClienteRestController.java
+++ desafios/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/controller/ClienteRestController.java
@@ -12,36 +12,42 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Clientes")
 @RequestMapping("clientes")
 public class ClienteRestController {
-    @Autowired
     private ClienteService clienteService;
 
+    @Autowired
+    public ClienteRestController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @Autowired
     @GetMapping
-    @Operation(description = "Lista de todos os clientes")
+    @Operation(summary = "Todos os clientes", description = "Lista de todos os clientes")
     public ResponseEntity<Iterable<Cliente>> buscarTodos() {
         return ResponseEntity.ok(clienteService.buscarTodos());
     }
+
     @GetMapping("/{id}")
-    @Operation(description = "Lista cliente por id")
+    @Operation(summary = "Cliente por Id", description = "Busca cliente por Id")
     public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(clienteService.buscarPorId(id));
     }
 
     @PostMapping
-    @Operation(description = "Cadastra novo cliente")
+    @Operation(summary = "Cadastro cliente", description = "Cadastra um novo cliente")
     public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
         clienteService.inserir(cliente);
         return ResponseEntity.ok(cliente);
     }
 
     @PutMapping("/{id}")
-    @Operation(description = "Atualiza informações do cliente selecionado por id")
+    @Operation(summary = "Atualiza cliente", description = "Atualiza informações do cliente selecionado por Id")
     public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
         clienteService.atualizar(id, cliente);
         return ResponseEntity.ok(cliente);
     }
 
     @DeleteMapping("/{id}")
-    @Operation(description = "Apaga o cadastro do cliente selecionado por id")
+    @Operation(summary = "Apaga cliente", description = "Apaga o cadastro do cliente selecionado por id")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         clienteService.deletar(id);
         return ResponseEntity.ok().build();
