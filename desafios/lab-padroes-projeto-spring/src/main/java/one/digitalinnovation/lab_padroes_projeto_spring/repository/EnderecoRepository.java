fd34dd6b3f164304e46d16a6c8d4ef54d11eada3
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-24 13:32:02 -0300

    feat: Endereco class e EnderecoRepository

---
+++ desafios/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.lab_padroes_projeto_spring.repository;
+
+import one.digitalinnovation.lab_padroes_projeto_spring.model.Endereco;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+}
