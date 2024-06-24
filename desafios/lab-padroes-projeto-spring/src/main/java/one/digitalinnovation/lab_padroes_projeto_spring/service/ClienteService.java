ad608643740cf5cfd1da1912047d96cb62960113
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-24 13:52:25 -0300

    feat: ClienteService

---
+++ desafios/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/service/ClienteService.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.lab_padroes_projeto_spring.service;
+
+import one.digitalinnovation.lab_padroes_projeto_spring.model.Cliente;
+
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+    Cliente buscarPorId(Long id);
+    void inserir(Cliente cliente);
+    void atualizar(Long id, Cliente cliente);
+    void deleter(Long id);
+}
