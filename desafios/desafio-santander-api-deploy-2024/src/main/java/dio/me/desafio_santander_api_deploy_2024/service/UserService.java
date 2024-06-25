9b7e52a226aa6e8987a087f7be30bf47b59f9c0b
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-25 10:04:19 -0300

    feat: UserService interface

---
+++ desafios/desafio-santander-api-deploy-2024/src/main/java/dio/me/desafio_santander_api_deploy_2024/service/UserService.java
@@ -0,0 +1,6 @@
+package dio.me.desafio_santander_api_deploy_2024.service;
+
+import dio.me.desafio_santander_api_deploy_2024.model.User;
+
+public interface UserService extends CrudService<Long, User> {
+}
