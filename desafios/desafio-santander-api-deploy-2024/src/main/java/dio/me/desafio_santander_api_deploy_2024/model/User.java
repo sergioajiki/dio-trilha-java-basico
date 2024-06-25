c7db39e062be9771ff4e4e52aef402cd87be0d68
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-24 23:18:25 -0300

    feat: entity User

---
+++ desafios/desafio-santander-api-deploy-2024/src/main/java/dio/me/desafio_santander_api_deploy_2024/model/User.java
@@ -0,0 +1,69 @@
+package dio.me.desafio_santander_api_deploy_2024.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToOne
+    private Account account;
+    @OneToOne
+    private Card card;
+    @OneToMany
+    private List<Feature> featureList;
+    @OneToMany
+    private List<News> news;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<Feature> getFeatureList() {
+        return featureList;
+    }
+
+    public void setFeatureList(List<Feature> featureList) {
+        this.featureList = featureList;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+}
