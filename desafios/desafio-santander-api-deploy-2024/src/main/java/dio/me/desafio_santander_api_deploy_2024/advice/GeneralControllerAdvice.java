6becb466ef06f316ee7d797c96e7ad0b3c065f99
Sergio Ajiki <sergioajk@hotmail.com>
2024-06-26 18:00:19 -0300

    feat: adicionando comentarios openAPI

--- desafios/desafio-santander-api-deploy-2024/src/main/java/dio/me/desafio_santander_api_deploy_2024/advice/GeneralControllerAdvice.java
+++ desafios/desafio-santander-api-deploy-2024/src/main/java/dio/me/desafio_santander_api_deploy_2024/advice/GeneralControllerAdvice.java
@@ -1,6 +1,8 @@
 package dio.me.desafio_santander_api_deploy_2024.advice;
 
+import dio.me.desafio_santander_api_deploy_2024.exception.BusinessException;
 import dio.me.desafio_santander_api_deploy_2024.exception.NotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,8 +16,17 @@ public class GeneralControllerAdvice {
     private final Logger LOOGER = LoggerFactory.getLogger(GeneralControllerAdvice.class);
 
     @ExceptionHandler
-    public ResponseEntity<String> handleNotFoundException(NotFoundException exception){
+    public ResponseEntity<String> handleNotFoundException(NotFoundException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
+    @ExceptionHandler
+    public ResponseEntity<String> handleBusinessException(BusinessException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
 }
--- desafios/desafio-santander-api-deploy-2024/src/main/java/dio/me/desafio_santander_api_deploy_2024/controller/UserController.java
+++ desafios/desafio-santander-api-deploy-2024/src/main/java/dio/me/desafio_santander_api_deploy_2024/controller/UserController.java
@@ -3,6 +3,8 @@ package dio.me.desafio_santander_api_deploy_2024.controller;
 import dio.me.desafio_santander_api_deploy_2024.controller.dto.UserDto;
 import dio.me.desafio_santander_api_deploy_2024.model.User;
 import dio.me.desafio_santander_api_deploy_2024.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Tag(name = "Users")
 @RequestMapping("/users")
 public class UserController {
     private UserService userService;
@@ -23,6 +26,7 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "All users", description = "List of all users")
     public ResponseEntity<List<UserDto>> findAll() {
         List<User> allUsers = userService.findAll();
         List<UserDto> allUserDto = allUsers.stream().map(UserDto::new).collect(Collectors.toList());
@@ -30,12 +34,14 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "User by id", description = "Search user by id")
     public ResponseEntity<UserDto> findById(@PathVariable Long id) {
         User user = userService.findById(id);
         return ResponseEntity.ok(new UserDto(user));
     }
 
     @PostMapping
+    @Operation(summary = "Register user", description = "Register a user")
     public ResponseEntity<UserDto> create(@RequestBody UserDto userDto) {
         User userToCreate = userDto.toModel();
         userService.create(userToCreate);
@@ -48,6 +54,7 @@ public class UserController {
     }
 
     @PutMapping("/id")
+    @Operation(summary = "Update user", description = "Update a user by id")
     public ResponseEntity<UserDto> update(@PathVariable Long id, @RequestBody UserDto userDto) {
         User userToUpdate = userDto.toModel();
         userService.update(id, userToUpdate);
@@ -55,6 +62,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete user", description = "Delete a user by id")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
